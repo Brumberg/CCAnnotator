c80a93d69f3b3c99053f7b563a9418e276d0022c
Oliver Brumberg <oliver.brumberg@endress.com>
2025-04-24 10:43:22 +0200

    Update of inline styles

--- VersionInfo.cpp
+++ VersionInfo.cpp
@@ -1,4 +1,4 @@
 
 #include "VersionInfo.h"
 #include <string>
-const std::string CClangOneVersion::m_c8Version = R"(<style> .tiny-text{font-size: 10px; } </style><p class="tiny-text">Code annotated by ClangOne - ClangOne version 00.00.90</p>)";
+const std::string CClangOneVersion::m_c8Version = R"(<p style='font-size: 10px'>Code annotated by ClangOne - ClangOne version 00.00.90</p>)";
